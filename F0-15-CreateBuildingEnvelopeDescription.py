6bea5c0742840cffd8b764bdee58c9e21f9f1f5e
dreierpawel <dreierpawel@gmail.com>
2021-12-20 15:42:35 +0100

    Update BE Scripts

--- F0-15-CreateBuildingEnvelopeDescription.py
+++ F0-15-CreateBuildingEnvelopeDescription.py
@@ -1,38 +1,46 @@
-import sys
-import clr
+# P.DREIER ©  github.com/dreierpawel
 import math
+import clr
+import sys
 
-# Import DocumentManager
+sys.path.append('C:\Program Files (x86)\IronPython 2.7\Lib')
+import System
+from System import Array
+from System.Collections.Generic import *
+
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+
+clr.AddReference("RevitNodes")
+import Revit
+
+clr.ImportExtensions(Revit.Elements)
+clr.ImportExtensions(Revit.GeometryConversion)
 clr.AddReference("RevitServices")
 import RevitServices
 from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
 
-clr.AddReference("RevitNodes")
-import Revit
-
-clr.ImportExtensions(Revit.GeometryConversion)
-
-# Import RevitAPI
 clr.AddReference("RevitAPI")
+clr.AddReference("RevitAPIUI")
+import Autodesk
 from Autodesk.Revit.DB import *
+from Autodesk.Revit.UI import *
 
 # Add List Object
 from System.Collections.Generic import List
+import collections
 
-import System
-import System.Reflection
 
 from collections import defaultdict
 
-# Add List Object
-from System.Collections.Generic import List
-
-
-### Definitions
 
 def to_cm(value):
     return UnitUtils.ConvertFromInternalUnits(value, UnitTypeId.Centimeters)
+
+
+def getInstanceLocationPoint(instance):
+    return to_cm(instance.Location.Point.Z)
 
 
 def extract(lst, i):
@@ -41,6 +49,50 @@ def extract(lst, i):
 
 def flatten(t):
     return [item for sublist in t for item in sublist]
+
+
+def filter_building_envelopes_elements_from_selected_elements(elements):
+    global doc
+    # Filter walls from elements input
+    vertical_building_envelope = []
+    horizontal_building_envelope = []
+    for e in elements:
+        e_type = e.GetType()
+        h_bool = e_type == Floor or (e_type == FootPrintRoof) or e_type == Ceiling
+        if e.GetType() == Wall:
+            vertical_building_envelope.append(e)
+        if h_bool:
+            if e.GetType() == FootPrintRoof:
+                # Roofs
+                level_id = e.get_Parameter(BuiltInParameter.ROOF_BASE_LEVEL_PARAM).AsElementId()
+                level = doc.GetElement(level_id)
+                level_elevation = to_cm(level.Elevation)
+                level_offset = to_cm(e.get_Parameter(BuiltInParameter.ROOF_LEVEL_OFFSET_PARAM).AsDouble())
+                element_location = level_elevation + level_offset
+                e = e.RoofType
+            else:
+                # Floors and Ceilings
+                level_id = e.get_Parameter(BuiltInParameter.LEVEL_PARAM).AsElementId()
+                level = doc.GetElement(level_id)
+                level_elevation = to_cm(level.Elevation)
+                try:
+                    level_offset = to_cm(e.get_Parameter(BuiltInParameter.FLOOR_HEIGHTABOVELEVEL_PARAM).AsDouble())
+                    e = e.FloorType
+                except:
+                    level_offset = to_cm(e.get_Parameter(BuiltInParameter.CEILING_HEIGHTABOVELEVEL_PARAM).AsDouble())
+                    e_id = e.GetTypeId()
+                    e = doc.GetElement(e_id)
+                element_location = level_elevation + level_offset
+                horizontal_building_envelope.append((e, element_location))
+    # Assign your output to the OUT variable.
+    try:
+        if h_bool:
+            horizontal_building_envelope = extract(sorted(horizontal_building_envelope , key=lambda x: x[1], reverse=True), 0)
+    except:
+        if h_bool:
+            horizontal_building_envelope = extract(horizontal_building_envelope, 0)
+
+    return (vertical_building_envelope, horizontal_building_envelope)
 
 
 def list_chop(lst):
@@ -239,7 +291,6 @@ def add_data_to_floor_tag(layer_tag, floors):
         fTypeFunction = "Horizontal"
         fTypeInfo = [fTypeName, fTypeId, fType, fTypeFunction, fTypeCode, fTypeStructure]
         floorsData.append(fTypeInfo)
-
     # Floors Reverse Structure of Last Wall
     output = []
     l = len(floorsData) - 1
@@ -258,9 +309,7 @@ def add_data_to_floor_tag(layer_tag, floors):
             output.append(w)
         else:
             output.append(w)
-
     # Extract and make strings from data
-
     partitionCode = "/".join(extract(output, 4))
     structureLayers = extract(output, 5)
     materials = flatten(extract(structureLayers, 0))
@@ -289,21 +338,16 @@ def add_data_to_floor_tag(layer_tag, floors):
         matCode = ".".join(matCode)
         materialsNames.append(matName)
         materialsCodes.append(matCode)
-
     # Wall Codes
     floorCodes = extract(output, 4)
     partitionStructure = extract(extract(output, 5), 1)
-
     new_thickness = []
     for ti, slope_min in zip(thickness, slope_min):
         new_thickness.append((slope_min + str(ti)))
-
     thickness = new_thickness
-
     materialsCodes = add_enter(materialsCodes)
     materialsNames = add_enter(materialsNames)
     thickness = add_enter(add_cm_to_str(thickness))
-
     floor_lst = []
     floor_num = []
     for elem, code in zip(partitionStructure, floorCodes):
@@ -311,17 +355,12 @@ def add_data_to_floor_tag(layer_tag, floors):
         floor_Codes = code + ("\n" * num)
         floor_lst.append(floor_Codes)
         floor_num.append(num)
-
     # Get floor ID
-
     floorTypes_lst = extract(output, 2)
-
     id_lst = []
-
     for i in floorTypes_lst:
         floorId = str(i.Id)
         id_lst.append(floorId)
-
     # Change values of tags
     TransactionManager.Instance.EnsureInTransaction(doc)
     layer_tag.LookupParameter("KOD ZBIORCZY PRZEGRODY BUDOWLANEJ").Set("/".join(floorCodes).replace("/", " / "))
@@ -335,41 +374,43 @@ def add_data_to_floor_tag(layer_tag, floors):
     TransactionManager.Instance.TransactionTaskDone()
 
 
-### Inputs
+def add_data_to_building_envelope_tag(layer_tag, be_list):
+    # vertical_building_envelope
+    elements = filter_building_envelopes_elements_from_selected_elements(be_list)[0]
+    # horizontal_building_envelope
+    horizontal_elements = filter_building_envelopes_elements_from_selected_elements(be_list)[1]
+    #
+    wall_function_dict = {
+        "Curtain Wall": -1,
+        "Interior": 3,
+        "Exterior": 0,
+        "Foundation": 2,
+        "Retaining": 4,
+        "Soffit": 5,
+        "Coreshaft": 1
+    }
+    try:
+        # Filter walls from elements input
+        walls = []
+        for e in elements:
+            if e.GetType() == Wall:
+                function_number = wall_function_dict.get(str(e.WallType.Function))
+                wTypeName = e.WallType.FamilyName
+                if wTypeName == "Curtain Wall":
+                    function_number = wall_function_dict.get("Curtain Wall")
+                walls.append((e, function_number))
 
-elements = UnwrapElement(IN[0])
+        walls = sort_walls_Ext_to_Int(walls)
+        add_data_to_wall_tag(layer_tag, walls)
+
+    except:
+        add_data_to_floor_tag(layer_tag, horizontal_elements)
+
+
+# Inputs
+elements_selected = UnwrapElement(IN[0])
 layer_tag = UnwrapElement(IN[1])
-horizontal_elements = UnwrapElement(IN[2])
-
-### Body
-
+# Code
 doc = DocumentManager.Instance.CurrentDBDocument
-
-wall_function_dict = {
-    "Curtain Wall": -1,
-    "Interior": 3,
-    "Exterior": 0,
-    "Foundation": 2,
-    "Retaining": 4,
-    "Soffit": 5,
-    "Coreshaft": 1
-}
-
-try:
-    # Filter walls from elements input
-    walls = []
-    for e in elements:
-        if e.GetType() == Wall:
-            function_number = wall_function_dict.get(str(e.WallType.Function))
-            wTypeName = e.WallType.FamilyName
-            if wTypeName == "Curtain Wall":
-                function_number = wall_function_dict.get("Curtain Wall")
-            walls.append((e, function_number))
-
-    walls = sort_walls_Ext_to_Int(walls)
-    add_data_to_wall_tag(layer_tag, walls)
-
-except:
-    add_data_to_floor_tag(layer_tag, horizontal_elements)
-
-OUT = layer_tag
+#
+OUT = add_data_to_building_envelope_tag(layer_tag, elements_selected)
